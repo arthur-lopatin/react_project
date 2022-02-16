d2a1c9c7fb9a2fb4f451e86bd6105c2036d9a05d
Arthur <arthur.lopatin@yandex.ru>
2022-02-16 20:09:49 +0300

    push

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./Components/Header";
+// import Card from "./Components/Card";
 
 const App = () => {
   return (
--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,23 +1,23 @@
 import React from "react";
 // @ts-ignore
-// import styles from "./Header.module.scss";
+import styles from "./Header.module.scss";
 
 const Header: React.FC = () => {
   return (
-    <header className="d-flex justify-between align-center p-40">
-      <div className="d-flex align-center">
+    <header className={styles.header}>
+      <div className={styles.logo}>
         <img
           style={{ width: "40px", height: "40px" }}
           src="/img/logo.png"
           alt="Logo"
         />
         <div>
-          <h3 className="text-uppercase">React Sneakers</h3>
-          <p className="opacity-5">Магазин лучших кроссовок</p>
+          <h3>React Sneakers</h3>
+          <p>Магазин лучших кроссовок</p>
         </div>
       </div>
-      <ul className="d-flex">
-        <li className="mr-30 cu-p">
+      <ul>
+        <li className={styles.cart}>
           <img
             style={{ width: "18px", height: "18px" }}
             src="/img/cart.svg"
