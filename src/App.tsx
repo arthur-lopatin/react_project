4347c3628b98530b67d7b16b75cac85a997e7aed
Arthur <arthur.lopatin@yandex.ru>
2022-02-16 20:21:17 +0300

    push

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,15 @@ const App = () => {
   return (
     <div className="wrapper clear">
       <Header />
+      <div className="content">
+        <div className="content_item">
+          <h1>Все кроссовки</h1>
+          <div className="search-block">
+            <img src="/img/search.svg" alt="Search" />
+            <input placeholder="Поиск..." />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
